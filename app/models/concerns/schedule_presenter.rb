5124dd70027e10d412160e62d83d76afdafdae8b
Egor Iskrenkov <mrblazon@gmail.com>
2019-01-22 01:00:39 +0300

    Week schedule added

--- app/models/concerns/schedule_presenter.rb
+++ app/models/concerns/schedule_presenter.rb
@@ -1,19 +1,43 @@
 module SchedulePresenter
   WEEKENDS_IDS = [0, 6].freeze
 
-  def weekday_schedule_for(weekday_id)
-    return t('.weekday.no_pairs', weekday: weekday_name(weekday_id)) if WEEKENDS_IDS.include? weekday_id
+  def present_schedule_for_weekday(id)
+    return t('.weekday.no_pairs', weekday: weekday_name(weekday_id)) if WEEKENDS_IDS.include? id
 
-    message = t('.weekday.schedule_for', weekday: weekday_name(weekday_id))
-    Weekday.find(weekday_id).pairs.each do |pair|
-      message << "\n#{pair.start_time} - #{pair.name}, #{t('.weekday.place', number: pair.place)}"
+    message = t('.weekday.schedule_for', weekday: weekday_name(id))
+    message << fetch_weekday(id)
+  end
+
+  def present_week_schedule
+    schedule = t('.schedule.header', semester: current_semester)
+    Weekday.all.each do |weekday|
+      schedule << "#{weekday.name} #{fetch_weekday(weekday.id)}\n\n" if weekday.pairs.present?
     end
-    message
+    schedule
   end
 
   private
 
+  def fetch_weekday(id)
+    weekday_schedule = ''
+    Weekday.find(id).pairs.each do |pair|
+      weekday_schedule << "\n#{pair.start_time} - #{pair.name}"
+      weekday_schedule << ", #{fetch_kind(pair)}" if fetch_kind(pair)
+      weekday_schedule << ", #{pair.place}" if pair.place
+    end
+    weekday_schedule
+  end
+
+  def fetch_kind(pair)
+    kinds = { 0 => 'Лекция', 1 => 'Практика', 2 => 'Лаба', 3 => nil, 4 => 'Семинар', 5 => nil }
+    kind = kinds[pair.kind]
+  end
+
   def weekday_name(number)
     Weekday.all.pluck(:name)[number]
+  end
+
+  def current_semester
+    Schedule.last.semester
   end
 end
