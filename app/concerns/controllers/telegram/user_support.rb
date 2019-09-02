9f6f02201c0c741e343bce59125b49a6622b949e
Egor Iskrenkov <mrblazon@gmail.com>
2019-09-02 08:53:43 +0300

    TelegramUser fix

--- app/concerns/controllers/telegram/user_support.rb
+++ app/concerns/controllers/telegram/user_support.rb
@@ -4,9 +4,9 @@ module Controllers::Telegram::UserSupport
   def telegram_user
     @telegram_user ||= begin
       TelegramUser.find_or_create_by(telegram_id: from['id']) do |user|
-        user.username = from['username']
-        user.first_name = from['first_name']
-        user.last_name = from['last_name']
+        user.username = from.fetch('username', '')
+        user.first_name = from.fetch('first_name', '')
+        user.last_name = from.fetch('last_name', '')
       end
     end
   end
